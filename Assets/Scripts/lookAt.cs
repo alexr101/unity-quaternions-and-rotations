c6568cc7d56565412c73924b49125af334763adb
alexr101 <alex_r_101@hotmail.com>
2018-10-28 23:33:15 -0400

    create reusable object rotator script

---
+++ Assets/Scripts/lookAt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAt : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.rotation = Quaternion.Euler(0, 100, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/rotator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+
+    public bool xRotation;
+    public float xSpeed;
+    public bool yRotation;
+    public float ySpeed;
+    public bool zRotation;
+    public float zSpeed;
+
+    private float x = 0;
+    private float y = 0;
+    private float z = 0;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(xRotation)
+            x += xSpeed; 
+        if(yRotation)
+            y += ySpeed;
+        if(zRotation)
+            z += zSpeed;
+
+        transform.rotation = Quaternion.Euler(x, y, z);
+
+
+	}
+}
