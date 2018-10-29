1abab9ca0b62ed6a08aaaad43c60fe59f6e92a33
alexr101 <alex_r_101@hotmail.com>
2018-10-29 00:14:38 -0400

    add object look at script + object look at mouse script

---
+++ Assets/Scripts/lookAtMouse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtMouse : MonoBehaviour {
+	void Update () {
+        float mouseX = Input.GetAxis("Mouse X");
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += mouseX * 5;
+        transform.localEulerAngles = newRotation;
+	}
+}
---
+++ Assets/Scripts/lookAtObj.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtObj : MonoBehaviour {
+    public Transform target;
+	
+	void Update () {
+        Vector3 relativePos = target.position - transform.position;
+        transform.rotation = Quaternion.LookRotation(relativePos);
+	}
+}
